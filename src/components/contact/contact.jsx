3984bfa197a7945479c894286260e2145a4b82d5
Dac099 <davidcejazapata@gmail.com>
2023-10-17 13:41:41 -0600

    Contact card section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -8,18 +8,80 @@ export function Contact(){
 
   return (
     <article 
-      className={styles.card}
+      className={showContent
+        ? styles.card
+        : `${styles.card} ${styles.card_hidden}`
+      }
       onMouseEnter={() => {
         setShowContent(true);
-        setCurrentCard('Contacts');
+        setCurrentCard('Contact');
       }}
       onMouseLeave={() => {
         setShowContent(false);
         setCurrentCard(null);
       }}
     >
-      {showContent &&
-        <p>Contact</p>
+      {showContent && currentCard == 'Contact' &&
+        <section className={styles.container}>
+          <h2 
+            className={styles.main_title}
+          >
+            Contact <span>me</span>
+          </h2>
+
+          <div className={styles.content}>
+            <div>
+              <p className={styles.text}>
+                You can find me here
+              </p>
+
+                <nav className={styles.contact}>
+                  <ul>
+                    <li> 
+                      <a 
+                        href="https://www.linkedin.com/in/david-ceja-zapata/"
+                        target='_blank'
+                        rel={'noreferrer'}
+                      >LinkedIn
+                      </a> 
+                    </li>
+                    
+                    <li> 
+                      <a 
+                        href="https://github.com/Dac099"
+                        target='_blank'
+                        rel={'noreferrer'}
+                      >GitHub
+                      </a> 
+                    </li>
+                    
+                    <li> 
+                      <a 
+                        href="https://twitter.com/David_CA990"
+                        target='_blank'
+                        rel={'noreferrer'}
+                      >Twitter
+                      </a> 
+                    </li>
+                  </ul>
+                </nav>
+            </div>
+
+            <div>
+              <p className={styles.text}>
+                Also, you can download my CV
+              </p>
+
+              <a 
+                href="https://drive.google.com/file/d/1v6ka4eSpZ9odvTgXvzC78QbvhJXup0UX/view?usp=drive_link"
+                className={styles.cv}
+                target='_blank'
+                rel={'noreferrer'}
+              ></a>
+            </div>
+
+          </div>
+        </section>
       }
 
       {!showContent && currentCard === null &&
