244d591bd6d1f28e6aa5684ee2df152acf79f672
Dac099 <davidcejazapata@gmail.com>
2023-10-16 20:13:03 -0600

    About content card

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,6 +1,8 @@
 import styles from './about.module.css';
 import { useState, useContext } from 'react';
 import { ViewCardContext } from '../../context/viewCard';
+import picture from '/picture.jpg';
+import reactIcon from '/react.svg';
 
 export function About(){
   const [ showContent, setShowContent ] = useState(false);
@@ -8,7 +10,10 @@ export function About(){
 
   return (
     <article 
-      className={styles.card}
+      className={!showContent
+        ? `${styles.card} ${styles.card_hidden}`
+        : styles.card
+      }
       onMouseEnter={() => {
         setShowContent(true);
         setCurrentCard('About');
@@ -18,8 +23,31 @@ export function About(){
         setCurrentCard(null);
       }}
     >
-      {showContent &&
-        <p>About</p>        
+      {showContent && currentCard === 'About' &&
+        <section className={styles.main}>
+          <h1 
+            className={styles.main_title}
+          >
+            React <span>front-end</span> developer
+          </h1>
+
+          <div className={styles.container_center}>
+            <div className={styles.main_images}>
+              <img 
+                src={picture}
+                className={styles.picture}
+              />
+              <img 
+                src={reactIcon}
+                className={styles.react}
+              />
+            </div>
+          </div>
+
+          <p className={styles.description}>
+            My name is <span className={styles.name}>David Ceja Zapata</span>, and I am a React Front-end developer. My focus is to create interfaces with the best development practices, as well as leaving a lasting impression. From responsive design to performance optimization.
+          </p>
+        </section>        
       }
 
       {!showContent && currentCard === null &&
